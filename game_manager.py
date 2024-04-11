9950db1dd1c98389adc9c0e004f9f16c059b075c
letuanvu <ngovipmo1@gmail.com>
2024-04-11 21:05:23 +0700

    a

--- game_manager.py
+++ game_manager.py
@@ -242,6 +242,8 @@ def renderVD():
     relative_path_upload_img = "static/upload_img/"
     frame = cv2.imread(os.path.join(absolute_path, relative_path_chessboard_0))
 
+    print(os.listdir(os.path.join(absolute_path, relative_path_video)))
+
     video = cv2.VideoWriter(os.path.join(absolute_path, relative_path_video), cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 1, frame.shape[:2])
     for i in range(len(os.listdir(os.path.join(absolute_path, relative_path_upload_img)))):
         relative_path_chessboard = f"static/upload_img/chessboard{i}.png"
@@ -249,7 +251,7 @@ def renderVD():
     video.release()
     # print(os.listdir(os.path.join(absolute_path, ".venv")))
 
-    # print(os.path.join(absolute_path, relative_path_video))
+    print(os.listdir(os.path.join(absolute_path, relative_path_video)))
 
     # chèn nhạc vô video
     # my_clip = mpe.VideoFileClip(os.path.join(absolute_path, relative_path_video))
