0a350bcda91cf64574cbdc3663d65019795291a6
letuanvu <ngovipmo1@gmail.com>
2024-04-11 21:27:10 +0700

    a

--- game_manager.py
+++ game_manager.py
@@ -253,7 +253,7 @@ def renderVD():
 
     print(os.listdir(os.path.join(absolute_path, "static/upload_video")))
     print(os.listdir(os.path.join(absolute_path, "static")))
-    print(os.listdir(absolute_path))
+    print(absolute_path)
 
     # chèn nhạc vô video
     # my_clip = mpe.VideoFileClip(os.path.join(absolute_path, relative_path_video))
