79b596acd318015d6d70b1ce7d63b50b023e4d95
letuanvu <ngovipmo1@gmail.com>
2024-04-11 20:37:54 +0700

    a

--- game_manager.py
+++ game_manager.py
@@ -242,14 +242,14 @@ def renderVD():
     relative_path_upload_img = "static/upload_img/"
     frame = cv2.imread(os.path.join(absolute_path, relative_path_chessboard_0))
 
+    print(os.listdir(os.path.join(absolute_path, "static/upload_video")))
+
     video = cv2.VideoWriter(os.path.join(absolute_path, relative_path_video), 0, 1, frame.shape[:2])
     for i in range(len(os.listdir(os.path.join(absolute_path, relative_path_upload_img)))):
         relative_path_chessboard = f"static/upload_img/chessboard{i}.png"
-        print(relative_path_chessboard)
+        print(os.path.join(absolute_path, relative_path_chessboard))
         video.write(cv2.imread(os.path.join(absolute_path, relative_path_chessboard)))
-    # video.release()
-
-    print(frame)
+    video.release()
     print(os.listdir(os.path.join(absolute_path, "static/upload_video")))
     print(os.listdir(os.path.join(absolute_path, "static")))
     print(os.listdir(absolute_path))
