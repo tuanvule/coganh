69c55adce280d6ac09d5e9896ed83c1b03a33efd
tuanvule <ngovipmo1@gmail.com>
2024-04-18 08:46:33 +0700

    a

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ def update_rank_board():
 
     return users
 
-# if __name__ == '__main__':
-#     open_browser = lambda: webbrowser.open_new("http://127.0.0.1:5000")
-#     Timer(1, open_browser).start()
-#     app.run(port=5000, debug=True, use_reloader=False)
+if __name__ == '__main__':
+    open_browser = lambda: webbrowser.open_new("http://127.0.0.1:5000")
+    Timer(1, open_browser).start()
+    app.run(port=5000, debug=True, use_reloader=False)
