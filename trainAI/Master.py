85ac52ffe8b6f87fdae6837c6e6951ed64afe099
tuanvule <ngovipmo1@gmail.com>
2024-05-09 09:12:01 +0700

    new

--- trainAI/Master.py
+++ trainAI/Master.py
@@ -1,21 +1,24 @@
 import os
 
-def ganh_chet(move, opp_pos, your_side, opp_side, board):
+def ganh_chet(move, opp_pos, your_board, opp_board):
 
     valid_remove = []
 
     for x0, y0 in opp_pos:
         dx, dy = x0-move[0], y0-move[1]
         if -1<=dx<=1 and -1<=dy<=1 and (0 in (dx,dy) or (move[0]+move[1])%2==0):
-            if ((0<=move[0]-dx<=4 and 0<=move[1]-dy<=4 and board[move[1]-dy][move[0]-dx] == opp_side) or #ganh
-                (0<=x0+dx<=4 and 0<=y0+dy<=4 and board[y0+dy][x0+dx] == your_side)): # chet
+            ganhx, ganhy = move[0]-dx, move[1]-dy
+            chetx, chety = x0+dx, y0+dy
+            if ((0<=ganhx<=4 and 0<=ganhy<=4 and opp_board&(1<<5*(4-ganhy)+(4-ganhx)) != 0) or # ganh
+                (0<=chetx<=4 and 0<=chety<=4 and your_board&(1<<5*(4-chety)+(4-chetx)) != 0)): # chet
                 valid_remove.append((x0, y0))
 
     for x, y in valid_remove:
-        board[y][x] = 0
+        opp_board ^= (1<<5*(4-y)+(4-x))
         opp_pos.remove((x, y))
 
-def vay(opp_pos, board):
+    return opp_board, opp_pos
+def vay(opp_pos, your_board, opp_board):
     
     for pos in opp_pos:
         if (pos[0]+pos[1])%2==0:
@@ -25,11 +28,10 @@ def vay(opp_pos, board):
         for move in move_list:
             new_valid_x = pos[0] + move[0]
             new_valid_y = pos[1] + move[1]
-            if 0<=new_valid_x<=4 and 0<=new_valid_y<=4 and board[new_valid_y][new_valid_x]==0:
-                return None
+            if 0<=new_valid_x<=4 and 0<=new_valid_y<=4 and (your_board|opp_board)&(1<<5*(4-new_valid_y)+(4-new_valid_x)) == 0:
+                return opp_board, opp_pos
 
-    for x, y in opp_pos: board[y][x] = 0
-    opp_pos[:] = []
+    return 0, []
 
 def main(player):
     global move, board_pointF
@@ -42,14 +44,25 @@ def main(player):
         for j in range(5):
             board_pointF[i][j] = board_pointF[i][j]/max_pointF
 
-    minimax(player, Stopdepth=6)
+    try:
+        with open(os.path.join(dirname, "source_code/bit_board.txt")) as f:
+            cache = {(int(i.split(" ")[0]), int(i.split(" ")[1]), bool(i.split(" ")[2])):[float(i.split(" ")[3]), int(i.split(" ")[4])] for i in f.read().split("\n")[:-1]}
+    except: cache = {}
+
+    your_board = int("0b"+"".join("1" if ele == -1 else "0" for row in player.board for ele in row),2)
+    opp_board = int("0b"+"".join("1" if ele == 1 else "0" for row in player.board for ele in row),2)
+    minimax(player.your_pos, player.opp_pos, your_board, opp_board, Stopdepth=6, cache=cache)
+
+    # with open(os.path.join(dirname, f"source_code/bit_board.txt"), mode="w") as f:
+    #     [f.write( f"{' '.join(map(str, i[0])).replace('False', '0').replace('True', '1')} {' '.join(map(str, i[1]))}\n" ) for i in sorted(cache.items(), key=lambda i: i[1][1], reverse=True)]
 
     return move
 
-def minimax(player, depth=0, isMaximizingPlayer=True, Stopdepth=None, alpha=float("-inf"), beta=float("inf")):
+def minimax(your_pos, opp_pos, your_board, opp_board, depth=0, isMaximizingPlayer=True, Stopdepth=None, alpha=float("-inf"), beta=float("inf"), cache={}):
 
-    if depth == Stopdepth or (not player.your_pos) or (not player.opp_pos):
-        return (len(player.your_pos) - len(player.opp_pos))*50 + sum(board_pointF[y][x] for x, y in player.your_pos) - sum(board_pointF[y][x] for x, y in player.opp_pos) - depth
+    if (state := (your_board, opp_board, isMaximizingPlayer)) in cache and depth:
+        cache[state][1] += 1
+        return cache[state][0]
 
     board = player.board
     if isMaximizingPlayer:
@@ -71,22 +84,18 @@ def minimax(player, depth=0, isMaximizingPlayer=True, Stopdepth=None, alpha=floa
             if 0 <= invalid_move[0] <= 4 and 0 <= invalid_move[1] <= 4 and board[invalid_move[1]][invalid_move[0]] == 0 and \
                 (movement[0]*movement[1]==0 or (movement[0]*movement[1]!=0 and (pos[0]+pos[1])%2==0)) and alpha < beta:
 
-                pre_board = [i.copy() for i in board]
-                pre_your_pos = your_pos.copy()
-                pre_opp_pos = opp_pos.copy()
-
                 # Update move to board
                 board[invalid_move[1]][invalid_move[0]] = your_side
                 board[pos[1]][pos[0]] = 0
                 # Update move to positions
-                index_move = your_pos.index(pos)
-                your_pos[index_move] = invalid_move
+                your_new_pos = your_pos.copy()
+                your_new_pos[your_pos.index(pos)] = invalid_move
 
-                ganh_chet(invalid_move, opp_pos, your_side, opp_side, board)
-                if len(your_pos) > len(opp_pos) or len(your_pos) == len(opp_pos) == 3:
-                    vay(opp_pos, board)
+                opp_new_board, opp_new_pos = ganh_chet(invalid_move, opp_pos.copy(), your_new_board, opp_board)
+                if len(your_new_pos) > len(opp_new_pos) or len(your_new_pos) == len(opp_new_pos) == 3:
+                    opp_new_board, opp_new_pos = vay(opp_new_pos, your_new_board, opp_new_board)
 
-                value = minimax(player, depth+1, not isMaximizingPlayer, Stopdepth, alpha, beta)
+                value = minimax(opp_new_pos, your_new_pos, opp_new_board, your_new_board, depth+1, not isMaximizingPlayer, Stopdepth, alpha, beta, cache)
                 if depth == 0 and value > bestVal:
                     move["selected_pos"] = pos
                     move["new_pos"] = invalid_move
@@ -98,7 +107,5 @@ def minimax(player, depth=0, isMaximizingPlayer=True, Stopdepth=None, alpha=floa
                     beta = min(beta, value)
                     bestVal = min(bestVal, value)
 
-                # Undo move
-                board[:], your_pos[:], opp_pos[:] = pre_board, pre_your_pos, pre_opp_pos
-
+    cache[state] = [bestVal, 1]
     return bestVal
